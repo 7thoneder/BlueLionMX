1ca196f6c70b85e5bb07e5a2f2055c67662430f2
7thoneder <henx007@gmail.com>
2021-02-16 11:32:34 -0500

    Added and modified some data

--- sitecheck.py
+++ sitecheck.py
@@ -8,9 +8,9 @@ import subprocess
 sites = open('Sites.csv','r')
 
 for s in sites:
-	website = urllib2.urlopen(s).geturl()
-	code = str(urllib2.urlopen(s).getcode())
-	print('Website: ' + website + ' ---> ' + 'Code: ' + code)    
+	website = urllib2.urlopen(s).url
+	code = str(urllib2.urlopen(s).code)
+	print('Website: ' + website + ' ---> ' + 'Code: ' + code)
 
 	
 
