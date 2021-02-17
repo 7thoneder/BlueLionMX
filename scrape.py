7c4f6b588c96e92d2abc87065ce5b0fc9ceb049c
7thoneder <henx007@gmail.com>
2021-02-16 22:09:44 -0500

    Added comments

--- scrape.py
+++ scrape.py
@@ -2,12 +2,13 @@ import csv
 from bs4 import BeautifulSoup
 import requests
 
-
+#Create a file for the DB load
 sites = open('newwebsites.csv', 'w')
+
+#Opens website list and scrapes site Title, URL and MetaDescription
 urls = open('Sites.txt', 'r')
 websites = urls.read()
 site_list = websites.split()
-#urls = ['https://www.nhl.com','https://www.history.com','https://www.cnbc.com']
 for url in site_list:
     req = requests.get(url)
     req = req.content
@@ -16,6 +17,7 @@ for url in site_list:
     for meta in bs.find_all('meta'):
         if meta.get('name') == 'description':
             metadesc = meta.get('content')
+            #Writes site data to file that will be used for DB load
             sites.write(title + '\t' + url + '\t' + metadesc + '\n')
 
 sites.close
